72dfe31abbab01f2f92703cad4d7ddb90a3d578c
Florian Serraille <florianserraille@gmail.com>
2023-05-30 17:00:46 -0300

    Fix missing import

--- src/main/java/io/example/configuration/SecurityConfig.java
+++ src/main/java/io/example/configuration/SecurityConfig.java
@@ -11,6 +11,7 @@ import java.security.interfaces.RSAPublicKey;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
